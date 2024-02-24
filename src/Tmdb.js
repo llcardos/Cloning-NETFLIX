617092bdd6aaa5cc6acfd7e0b95f3416b94fad57
llcardos <lucas.businesss@yahoo.com>
2024-02-24 10:39:45 -0300

    'Cloning-Neflix'

---
+++ src/Tmdb.js
@@ -0,0 +1,52 @@
+const API_KEY = '50945eaa9d8d5e89d5275a5397677d96'
+const API_BASE = 'https://api.themoviedb.org/3/authentication'
+
+
+
+export default{
+    getHomeList: async () => {
+        return [
+            {
+                slug: 'originals',
+                title: 'Originais da Netflix',
+                itens: []
+            },
+            {
+                slug: 'trending',
+                title: 'Recomendados para você',
+                itens: []
+            },
+            {
+                slug: 'originals',
+                title: 'Originais da Netflix',
+                itens: []
+            },
+            {
+                slug: 'originals',
+                title: 'Originais da Netflix',
+                itens: []
+            },
+            {
+                slug: 'originals',
+                title: 'Originais da Netflix',
+                itens: []
+            },
+            {
+                slug: 'originals',
+                title: 'Originais da Netflix',
+                itens: []
+            },
+            {
+                slug: 'originals',
+                title: 'Originais da Netflix',
+                itens: []
+            },
+            {
+                slug: 'originals',
+                title: 'Originais da Netflix',
+                itens: []
+            }
+        ]
+    }
+
+}
